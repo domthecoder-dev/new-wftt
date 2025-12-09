e89ed38f7bcf56332720c7151692c1ca005fe79c
domthecoder-dev <dominic3lliott@gmail.com>
2025-12-10 00:14:32 +0200

    Fixes for new domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/new-wftt/',  // <-- CHANGE THIS TO YOUR REPO NAME            
+  base: '/',  // <-- CHANGE THIS TO YOUR REPO NAME            
   build: {
     outDir: 'dist',        // default, keep it
     assetsDir: 'assets',   // default, keep it
