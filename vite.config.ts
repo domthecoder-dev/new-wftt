3a47cf868aaea3bd2885510d19b7b07431b4d67a
domthecoder-dev <dominic3lliott@gmail.com>
2025-12-10 00:08:21 +0200

    Fix something

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',               // <-- important for gh-pages
+  base: '/new-wftt/',  // <-- CHANGE THIS TO YOUR REPO NAME            
   build: {
     outDir: 'dist',        // default, keep it
     assetsDir: 'assets',   // default, keep it
